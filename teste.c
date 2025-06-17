85046f2f79324cf5bcce9b873b3ed799a4cd367f
Arthur de França <arthurdfr2000@gmail.com>
2025-06-17 13:33:34 -0300

    Create teste c
    so criei

---
+++ teste.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    int x;
+    printf("Escreva um numero: ");
+    scanf("%d", &x);
+    printf("O numero escolhido foi: %d", x);
+    return 0;
+}
